c14b2a95297f8824a4510252fb4a73f057516520
andykenobi <andykenobi@gmail.com>
2017-03-20 11:44:49 -0300

    Adicionado novos components. Alterado a estrutura.

--- neocss.js
+++ neocss.js
@@ -2,14 +2,38 @@ $(document).ready(function() {
     // TOGGLE TOGGLE EFFECT
     // *data-toggle*
     // *data-target*
-    $("[data-toggle='toggle']").click(function() {
-        var target = $(this).data("target");
-        $(target).toggle();
+    $("[data-toggle='toggle']").each(function() {
+        $(this).click(function() {
+            var target = $(this).data("target");
+            $(target).toggle();
+        });
     });
+
     // TOGGLE COLLAPSE EFFECT
     // *class 'toggle'*
-    $("[data-toggle='collapse']").click(function() {
-        var target = $(this).data("target");
-        $(target).slideToggle();
+    $("[data-toggle='collapse']").each(function() {
+        $(this).click(function() {
+            var target = $(this).data("target");
+            $(target).slideToggle();
+        });
     });
+
+    $(".neo.button.close").each(function() {
+        $(this).click(function() {
+            $(this).parent().hide();
+        });
+    });
+    $(".neo.button.close-1").each(function() {
+        $(this).click(function() {
+            $(this).parent().parent().hide();
+        });
+    });
+
+
+    // CIRCLE BUTTON
+    // $(".circle").each(function() {
+        // $(this).css("height", $(this).css("width"));
+        // $(this).css("line-height", $(this).css("height"));
+        // $(this).css("vertical-align", "middle");
+    // });
 });
